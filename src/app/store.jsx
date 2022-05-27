7653b318df71960f3dfe2275ea6e5d82a4d20562
Joseph Maramba <josephmaramba50@gmail.com>
2022-05-27 13:21:13 +0300

    state persistence with redux-persist

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,65 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 import bulbReducer from "./bulbSlice";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, bulbReducer);
 
 export const store = configureStore({
   reducer: {
-    bulbs: bulbReducer,
+    bulbs: persistedReducer,
   },
+  middleware: [thunk],
 });
-
-// // action creators
-// export const toggleLights = () => {
-//   return {
-//     type: "TOGGLE_LIGHTS",
-//   };
-// };
-
-// export const addBulb = (payload) => {
-//   return {
-//     type: "ADD_BULB",
-//     payload,
-//   };
-// };
-
-// export const removeBulb = (payload) => {
-//   return {
-//     type: "REMOVE_BULB",
-//     payload,
-//   };
-// };
-
-// export const toggleLight = (payload) => {
-//   return {
-//     type: "TOGGLE_BULB_LIGHT",
-//     payload,
-//   };
-// };
-
-// // reducers
-// export const bulbReducer = (state, action) => {
-//   switch (action.type) {
-//     case "TOGGLE_LIGHTS":
-//       return {
-//         ...state,
-//         isLightsOn: !state.isLightsOn,
-//       };
-//     case "TOGGLE_BULB_LIGHT":
-//       const bulbItem = state.bulbs.find((bulb) => bulb.id === action.payload);
-//       return {
-//         ...state,
-//         bulbs: [...state.bulbs, { ...bulbItem, bulbItem.isOn = !bulbItem.isOn }],
-//       };
-//     case "ADD_BULB":
-//       return {
-//         ...state,
-//         bulbs: [...state.bulbs, action.payload],
-//       };
-//     case "REMOVE_BULB":
-//       return {
-//         ...state,
-//         bulbs: [...state.bulbs.filter((bulb) => bulb.id !== action.payload)],
-//       };
-//     default:
-//       return state;
-//   }
-// };
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import "./index.scss";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
